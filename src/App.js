3ad640209e05ed43e3e07612f301f3c0a12fcfed
MyroslavaNY <MyroslavaNY@gmail.com>
2022-01-27 06:27:43 +0200

    hw3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {link} from "react-router-dom";
 
 function App() {
   return (
     <div >
-
+      <h2> menu </h2>
+      <ul>
+        <li><link>home</link></li>
+        <li><link>layout</link>t</li>
+        <li><link>about</link></li>
+      </ul>
     </div>
   );
 }
