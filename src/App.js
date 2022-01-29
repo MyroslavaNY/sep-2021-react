c9121f70147eca7a329510193debfa1513e5bb4c
MyroslavaNY <MyroslavaNY@gmail.com>
2022-01-29 15:19:08 +0200

    hw4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import './App.css';
+import {useReducer} from "react";
+
+const reducer = (state, action) => {
+
+    switch (action.type) {
+        case 'inc':
+            return {...state, count1:state.count1+1}
+        case 'dec':
+            return { ...state, count1: state.count1-1}
+        case 'reset':
+            return {...state, count1: action.payload}
+
+        case 'inc2':
+            return {...state, count2:state.count2+2}
+        case 'dec2':
+            return { ...state, count2: state.count2-2}
+        case 'reset2':
+            return {...state, count2: action.payload}
+
+        case 'inc3':
+            return {...state, count3: state.count3 + 3}
+        case 'dec3':
+            return {...state, count3: state.count3 - 3}
+        case 'reset3':
+            return {...state, count3: action.payload}
+        // default:
+        //     throw new Error('Error')
+    }
+
+    return state;
+}
 
 function App() {
-  return (
+   const [state, dispatch] = useReducer (reducer, {count1:0, count2:0, count3:0});
+
+   return (
     <div >
 
+        <div>{state.count1}</div>
+        <button onClick={() => dispatch({type:'inc'})}>INC</button>
+        <button onClick={() => dispatch({type:'dec'})}>DEC</button>
+        <button onClick={() => dispatch({type:'reset', payload:8})}>RESET</button>
+
+        <div>{state.count2}</div>
+        <button onClick={() => dispatch({type:'inc2'})}>INC</button>
+        <button onClick={() => dispatch({type:'dec2'})}>DEC</button>
+        <button onClick={() => dispatch({type:'reset2', payload:5})}>RESET</button>
+
+        <div>{state.count3}</div>
+        <button onClick={() => dispatch({type:'inc3'})}>INC</button>
+        <button onClick={() => dispatch({type:'dec3'})}>DEC</button>
+        <button onClick={() => dispatch({type:'reset3', payload:0})}>RESET</button>
+
     </div>
-  );
+  )
 }
 
 export default App;
